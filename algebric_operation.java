7dd5a6d8026049b24c7cad7b35858f3595923213
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-07-22 20:10:37 +0530

    Create algebric_operation.java

---
+++ algebric_operation.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int a=20,b=30,sum,sub,mul,div,rem;
+        sum=a+b;
+        System.out.println(sum);
+        sub=a-b;
+        System.out.println(sub);
+        mul=a*b;
+        System.out.println(mul);
+        div=a/b;
+        System.out.println(div);
+        rem=a%b;
+        System.out.print(rem);
+    }
+}
