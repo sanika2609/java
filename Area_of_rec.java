58d40281918ff3ecd8000056ec0ca14594b57211
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-08-12 22:23:03 +0530

    Create Area_of_rec.java

---
+++ Area_of_rec.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Area_of_Rec {
+    public static void main(String[] args) {
+        int len,br,Area;
+        Scanner sc=new Scanner(System.in);
+        len=sc.nextInt();
+        br=sc.nextInt();
+        Area=len*br;
+        System.out.print("Area of rectangle is:"+Area);
+        
+    }
+}
