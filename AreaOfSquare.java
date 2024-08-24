5c34051842d401c25d771f55e13a302d16abe05a
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-08-24 21:26:23 +0530

    Create AreaOfSquare.java

---
+++ AreaOfSquare.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class AreaOfSquare{
+public static void main(String args[]){
+int side;
+Scanner sc=new Scanner(System.in);
+side=sc.nextInt();
+int Area=side*side;
+System.out.print("Area of square is:"+Area);
+}
+}
