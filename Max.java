59761a2d40c5eda9ca6a1915797632cc660c66df
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-08-25 22:44:05 +0530

    Create Max.java

---
+++ Max.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Max{
+public static void main(String []args)
+{
+int a,b;
+Scanner sc=new Scanner (System.in);
+a=sc.nextInt();
+b=sc.nextInt();
+if(a>b)
+{
+System.out.println("Greater no. is:"+a);
+}
+else
+{
+System.out.print("Greater no. is:"+b);
+}
+}
+}
