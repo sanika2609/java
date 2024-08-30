b62a79ee1a508a1fa1b28c33e9cd34c808e813ef
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-08-30 20:54:20 +0530

    Create square.py

---
+++ square.py
@@ -0,0 +1,12 @@
+import java.util.*;
+class SquareOfNum{
+    public static void main(String[] args){
+        int num,square;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter num");
+        num=sc.nextInt();
+        square=num*num;
+        System.out.print("Square of number is:"+square);
+        
+    }
+}
