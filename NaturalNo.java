fe6d67390a9f7e4750801aa833fe88eb0a6e1397
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-09-25 09:58:43 +0530

    Create NaturalNo.java

---
+++ NaturalNo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class NaturalNo{
+public static void main(String[] args){
+int n;
+Scanner sc=new Scanner (System.in);
+System.out.print("Enter the value of n");
+n=sc.nextInt();
+int i=1;
+while(i<=n){
+System.out.println(i);
+i++;
+}
+}
+}
