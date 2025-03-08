400c1f6037041d0d28b23cc635ecaadc5ac2add8
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2025-03-08 23:33:47 +0530

    Create String.java

---
+++ String.java
@@ -0,0 +1,9 @@
+public class StringExample {
+    public static void main(String[] args) {
+        // Declaration and Initialization
+        String str = "Hello, Java!";
+        
+        // Printing the String
+        System.out.println(str);
+    }
+}
